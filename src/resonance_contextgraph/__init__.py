0fb4fa06142cf2ec902b66db2ba4efc8374856c6
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-08-12 22:51:58 +0300

    Expose measurement scheduler API

--- src/resonance_contextgraph/__init__.py
+++ src/resonance_contextgraph/__init__.py
@@ -1,13 +1,15 @@
 """Resonance ContextGraph public runtime surface."""
 
-from .compiler import ContextCompiler, ContextRequest, CompiledContext
+from .compiler import CompiledContext, ContextCompiler, ContextRequest
 from .estimator import CellEvidence, EstimatorSpec, score_cell
+from .measurement import BalancedRoundRobin, MeasurementCell
 from .models import EvidenceClaim, EvidenceEvent, MissionSpec, ObserverReport
 from .reconcile import EventReconciler
 from .stopping import CheckpointObservation, PairStabilityStopper, StopDecision
 from .store import EvidenceStore
 
 __all__ = [
+    "BalancedRoundRobin",
     "CellEvidence",
     "CheckpointObservation",
     "CompiledContext",
@@ -18,6 +20,7 @@ __all__ = [
     "EvidenceEvent",
     "EvidenceStore",
     "EventReconciler",
+    "MeasurementCell",
     "MissionSpec",
     "ObserverReport",
     "PairStabilityStopper",
